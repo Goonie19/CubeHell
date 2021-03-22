e3b688031a63832097db0d524957de62b98473b5
Goonie19 <antonio.espinosabarrios98@gmail.com>
2021-03-22 17:43:59 +0100

    Initial commit

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+
+        private Vector2 _input;
+        private Rigidbody _rb;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _rb = GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            _input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+            
+        }
+    }
+}
+
